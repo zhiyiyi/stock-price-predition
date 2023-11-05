388c41d31eb1742613db350ea997e3f9c13874d1
vanislekahuna <ruiz.rivera93@gmail.com>
2023-11-04 19:56:01 -0700

    Attempting to fix the 'md_refresh.yml' pipeline

--- src/etl/refresh_data.py
+++ src/etl/refresh_data.py
@@ -4,12 +4,14 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
 from src.etl.extract_stock_data import refresh_data
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+from os import environ
 
 
+token = environ.get("TOKEN", None)
 ticker = "MSFT"
-load_dotenv(".env")
-token = os.getenv("motherduck_token")
+# load_dotenv(".env")
+# token = os.getenv("motherduck_token")
 
 if __name__ == "__main__":
     refresh_data(token, ticker)
