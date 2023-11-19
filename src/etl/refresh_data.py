83b15b885a398bfd9b342b031e1b4ec3d5eb6a8f
vanislekahuna <ruiz.rivera93@gmail.com>
2023-11-18 21:00:24 -0800

    Attempting to fix 'md_refresh.yml' by exporting token. Please do not merge

--- src/etl/refresh_data.py
+++ src/etl/refresh_data.py
@@ -9,12 +9,12 @@ from dotenv import load_dotenv
 
 # token = environ.get("motherduck_token", None)
 
-# get MotherDuck token from env
-load_dotenv(".env")
-token = os.getenv("motherduck_token")
-print(f"token: '{token}'")
+# # get MotherDuck token from env
+# load_dotenv(".env")
+# token = os.getenv("motherduck_token")
+# print(f"token: '{token}'")
 
 # token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoicnVpei5yaXZlcmE5My5nbWFpbC5jb20iLCJlbWFpbCI6InJ1aXoucml2ZXJhOTNAZ21haWwuY29tIiwidXNlcklkIjoiZGJiMmE2ZmYtMmZjNi00YjM2LTkzOGQtYzBmMzI5MWRlMWY4IiwiaWF0IjoxNjk5MTU2ODY3LCJleHAiOjE3MzA3MTQ0Njd9.thS_zI3creCQvb5ctSXaxt9nUvNvep7La2M-faJ0j3A"
 
 if __name__ == "__main__":
-    refresh_data(token)
+    refresh_data(motherduck_token)
