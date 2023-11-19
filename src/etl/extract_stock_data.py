d27d6bc00c93be8ec6b106a426cd6e0fd6c81d96
vanislekahuna <ruiz.rivera93@gmail.com>
2023-11-18 23:02:36 -0800

    Adding robustness to the code so that the pipeline doesn't break if we've already extracted the most recent stock data

--- src/etl/extract_stock_data.py
+++ src/etl/extract_stock_data.py
@@ -58,34 +58,42 @@ def refresh_data(token):
     ticker: str
         The ticker symbol for the stock you want to pull data from
     """
-    # connect to the database
-    con = duckdb.connect(f"md:?motherduck_token={token}")
-    recent_day_query = """
-    SELECT datetime
-    FROM stocks_clouddb.msft_data
-    ORDER BY datetime DESC
-    LIMIT 1
-    """
+    try:
+        # connect to the database
+        con = duckdb.connect(f"md:?motherduck_token={token}")
+        recent_day_query = """
+        SELECT datetime
+        FROM stocks_clouddb.msft_data
+        ORDER BY datetime DESC
+        LIMIT 1
+        """
 
-    latest_day = con.sql(recent_day_query).fetchall()[0][0]
-    today = datetime.today()
-    ticker = "MSFT"
+        latest_day = con.sql(recent_day_query).fetchall()[0][0]
+        today = datetime.today()
+        ticker = "MSFT"
 
-    # Extracting the historical market data (i.e. latest_day = (latest_day - timedelta(days=5)))
-    ticker = yf.Ticker(ticker)
+        # Extracting the historical market data (i.e. latest_day = (latest_day - timedelta(days=5)))
+        ticker = yf.Ticker(ticker)
 
-    recent_data = ticker.history(period="1d", interval="2m", start=latest_day, end=today)
-    recent_data = recent_data.rename_axis(["DateTime"]).reset_index().rename(columns={
-        "DateTime": "datetime",
-        "Open": "open_price",
-        "High": "day_high",
-        "Low": "day_low",
-        "Close": "close_price",
-        "Volume": "volume",
-        "Dividends": "dividends",
-        "Stock Splits": "stock_splits"
-    })
+        recent_data = ticker.history(period="1d", interval="2m", start=latest_day, end=today)
+        recent_data = recent_data.rename_axis(["DateTime"]).reset_index().rename(columns={
+            "DateTime": "datetime",
+            "Open": "open_price",
+            "High": "day_high",
+            "Low": "day_low",
+            "Close": "close_price",
+            "Volume": "volume",
+            "Dividends": "dividends",
+            "Stock Splits": "stock_splits"
+        })
 
-    con.sql("INSERT INTO stocks_clouddb.msft_data SELECT * FROM recent_data")
-    con.commit()
-    con.close()
+        con.sql("INSERT INTO stocks_clouddb.msft_data SELECT * FROM recent_data")
+        con.commit()
+        con.close()
+
+    except duckdb.duckdb.BinderException as e:
+        if "table msft_data has 8 columns but 7 values were supplied" in str(e):
+            print("The most recent stock data has already been extracted. Refreshing the database is not necessary.")
+        else:
+            # Handle other BinderExceptions or re-raise the exception
+            raise
