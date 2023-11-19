53885b52a1e3fc88b68fced4045d62a44cdbaf22
Zhiyi Chen <36960178+zhiyiyi@users.noreply.github.com>
2023-11-19 01:15:45 -0600

    Revert "reformat the datetime on x axis "

--- src/etl/refresh_data.py
+++ src/etl/refresh_data.py
@@ -4,14 +4,12 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
 from src.etl.extract_stock_data import refresh_data
-from os import environ
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-token = environ.get("TOKEN", None)
-print(f"token: '{token}'")
-# ticker = "MSFT"
-# load_dotenv(".env")
-# token = os.getenv("motherduck_token")
+
+ticker = "MSFT"
+load_dotenv(".env")
+token = os.getenv("motherduck_token")
 
 if __name__ == "__main__":
-    refresh_data(token)
+    refresh_data(token, ticker)
