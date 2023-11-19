abcc1d4b007690a4cf168cd574e28233a453db62
vanislekahuna <ruiz.rivera93@gmail.com>
2023-11-18 21:09:56 -0800

    Attempting to fix 'md_refresh.yml' by exporting token. Please do not merge

--- src/etl/refresh_data.py
+++ src/etl/refresh_data.py
@@ -6,12 +6,12 @@ from datetime import datetime, timedelta
 from extract_stock_data import refresh_data
 from dotenv import load_dotenv
 
-token = os.environ.get("motherduck_token")
+# token = os.environ.get("motherduck_token")
 
-# # get MotherDuck token from env
-# load_dotenv(".env")
-# token = os.getenv("motherduck_token")
-# print(f"token: '{token}'")
+# get MotherDuck token from env
+load_dotenv(".env")
+token = os.getenv("motherduck_token")
+print(f"token: '{token}'")
 
 # token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoicnVpei5yaXZlcmE5My5nbWFpbC5jb20iLCJlbWFpbCI6InJ1aXoucml2ZXJhOTNAZ21haWwuY29tIiwidXNlcklkIjoiZGJiMmE2ZmYtMmZjNi00YjM2LTkzOGQtYzBmMzI5MWRlMWY4IiwiaWF0IjoxNjk5MTU2ODY3LCJleHAiOjE3MzA3MTQ0Njd9.thS_zI3creCQvb5ctSXaxt9nUvNvep7La2M-faJ0j3A"
 
