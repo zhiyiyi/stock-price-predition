5f7db511cb1e8191d1afb4a8a54a8d81ed0e1c53
vanislekahuna <ruiz.rivera93@gmail.com>
2023-11-26 22:29:09 -0800

    Changing the md_refresh.yml code so as to avoid exposing the token

--- src/etl/refresh_data.py
+++ src/etl/refresh_data.py
@@ -4,11 +4,11 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
 from extract_stock_data import refresh_data
-from motherduck_token import motherduck_token 
+from motherduck_token import token 
 
 # Check if the token object is set
-if motherduck_token is None:
+if token is None:
     raise ValueError("Motherduck token not found. Make sure it is set in the environment.")
 
 if __name__ == "__main__":
-    refresh_data(motherduck_token)
+    refresh_data(token)
